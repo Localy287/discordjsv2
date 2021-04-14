dc7c5f3352d8f83d1a59a1aa31d42457fbe882d1
Localy287 <80224572+Localy287@users.noreply.github.com>
2021-04-13 20:41:00 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,7 @@
 module.exports =  async (client) => {
+    client.server_blacklist.set("689659773293297694", true);
+    client.server_blacklist.set("598731298609168414", true);
+    client.bypass_user.set("359835581456580618", true);
+    client.bypass_user.set("331615683173548033", true);
     console.log(`This is: ${client.user.tag}`)
 }
