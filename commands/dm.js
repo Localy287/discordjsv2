f728e7945696faff5158c8f3ce2dcb0b0e540bb9
Localy287 <80224572+Localy287@users.noreply.github.com>
2021-04-17 17:44:57 -0500

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,33 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (client, message, args) => {
+    if (args[0] == null){
+      return;
+    }
+    const user = client.users.cache.get(`args[0]`)
+    if (user == null){
+     return; 
+    }
+    const returnEmbed = new Discord.MessageEmbed()
+    .setColor("0x070707")
+    .setTitle("ICBM-C4 “Cluster Bomb”")
+    .setThumbnail("https://cdn.discordapp.com/attachments/828768110658977872/829892969443622963/mmm.png")
+    .setDescription("Hello, user this is a automated dm from ICBM-C4 “Cluster Bomb”")
+    user.send(returnEmbed)
+}
+
+module.exports.help = {
+  name: "dm",
+  description: "DM A user using the BOT."  
+}
+
+module.exports.requirements = {
+    userPerms: [],
+    clientPerms: [],
+    ownerOnly: false
+}
+
+module.exports.limits = {
+    rateLimit: 2,
+    cooldown: 1e4
+}
